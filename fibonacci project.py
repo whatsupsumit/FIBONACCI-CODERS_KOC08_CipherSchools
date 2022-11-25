c9c402e955baa43bdd33cbce8e368adf8664b793
whatsupsumit <115339907+whatsupsumit@users.noreply.github.com>
2022-11-25 19:01:52 +0530

    PYTHON PROJECT 12

---
+++ fibonacci project.py
@@ -0,0 +1,20 @@
+N = int(input("Enter the number you want to check: "))
+
+# variables for generating fibonacci sequence
+f3 = 0
+f1 = 1
+f2 = 1
+# 0 and 1 both are fibonacci numbers
+if (N == 0 or N == 1):
+    print("Given number is fibonacci number")
+
+else:
+    # generating the fibonacci numbers until the generated number is less than  N
+    while f3 < N:
+        f3 = f1 + f2
+        f2 = f1
+        f1 = f3
+    if f3 == N:
+        print("Given number is a valid fibonacci number")
+    else:
+        print("No it’s not a valid fibonacci number")
